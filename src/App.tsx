3eccae1c1d79567d8bdd68cb7f6a51315f95cabc
dhurwit <hurwit.d@husky.neu.edu>
2020-03-29 17:47:42 -0400

    switch to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          How did you get here??
+          How did you get here?
         </p>
       </header>
     </div>
